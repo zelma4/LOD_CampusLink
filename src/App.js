42d721207c98080ef7c418d3688c5d033790cd42
sasashko <oleksandr.romaniuk.sa.2022@lpnu.ua>
2023-05-04 20:07:31 +0300

    profile page template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import General from './components/General/General';
+// import General from './components/General/General';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './components/Profile/Profile'
 
 function App() {
   return (
     <div className="App">
-      <General />
+      {/* <General /> */}
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+const Profile = () => {
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>
+            <div> Profile </div>
+        </div>
+    )
+}
+
+export default Profile
