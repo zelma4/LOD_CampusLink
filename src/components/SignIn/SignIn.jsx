5229e3d6628c04fa9069143ec8d856680003b075
Dee6teg <132577054+Dee6teg@users.noreply.github.com>
2023-05-17 20:16:14 +0300

    SignIn Page Router

---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import './SignIn.css';
+import { Button } from 'react-bootstrap';
+
+
+const SignIn = () => {
+    return (
+      <div className="wrapper">
+        <Link to="general">
+          <Button variant="outline-secondary" className="sign-in-button">
+            SIGN IN
+          </Button>
+          <Button variant="outline-secondary" className="sign-up-button">
+            SIGN UP
+          </Button>
+        </Link>
+      <h1 className="CampusLink">CampusLink</h1>
+      <h2 className="CampusLink2">Say Hello To Coeducation</h2>
+      <span className="CampusLink3">
+        Alone we can do so little, together we can do so much
+      </span>
+      <div className="img-wrapper">
+        <img  src="/12.jpg"alt="12"
+        title="12" className="img" />
+      </div>
+    </div>
+    );
+  }
+
+  export default SignIn
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import General from './components/General';
+import SignIn from './components/SignIn';
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <SignIn/>
+  },
+  {
+    path: "/general", element: <General/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
